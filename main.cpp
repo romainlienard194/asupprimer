bc3b1a8f8b586c65740f141c337a21faef34707d
JLussiez <70692644+JLussiez@users.noreply.github.com>
2021-04-26 15:36:49 +0200

    test

---
+++ main.cpp
@@ -0,0 +1,22 @@
+#include <SFML/Graphics.hpp>
+
+
+int main(int argc, char **argv)
+{
+	sf::Window window(sf::VideoMode(800, 600), "Ma fenêtre swaggy");
+
+	// on fait tourner le programme jusqu'à ce que la fenêtre soit fermée
+	while (window.isOpen())
+	{
+		// on inspecte tous les évènements de la fenêtre qui ont été émis depuis la précédente itération
+		sf::Event event;
+		while (window.pollEvent(event))
+		{
+			// évènement "fermeture demandée" : on ferme la fenêtre
+			if (event.type == sf::Event::Closed)
+				window.close();
+		}
+	}
+
+	return 0;
+}
